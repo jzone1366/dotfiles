c7fd0f2812181d6744f16ab07a36eebcab3a3d9f
zonejm <jzone1229@gmail.com>
2018-03-22 20:59:07 -0700

    Add NVM to zsh exports. Add gopath to zsh exports. Update vimrc

--- link/zsh/exports.zsh
+++ link/zsh/exports.zsh
@@ -15,3 +15,12 @@ export EDITOR='vim'
 export LC_COLLATE=C
 
 # Projects and stuff
+
+# NVM Stuff
+export NVM_DIR="$HOME/.nvm"
+# This loads nvm
+[ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"  
+# This loads nvm bash_completion
+[ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion"  
+
+export GOPATH=$HOME/go
